7a1f85155d25dc03a862f4fd64b0e5d388d9111f
ksaubhagya23 <ksaubhagya23@gmail.com>
2026-06-14 21:22:06 +0530

    Add files via upload

---
+++ Day 13_Assignment/Day 13 (4) (1).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5], even = 0, odd = 0;
+
+    printf("Enter 5 elements:\n");
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < 5; i++) {
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even elements = %d\n", even);
+    printf("Odd elements = %d", odd);
+
+    return 0;
+}
